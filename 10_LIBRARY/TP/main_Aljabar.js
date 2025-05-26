f606ec342445df088e818dfb12a1f178a7822b36
Farelzy-Rz <73918830+farelzy@users.noreply.github.com>
2025-05-26 19:14:33 +0700

    Delete 10_LIBRARY directory

--- 10_LIBRARY/TP/main_Aljabar.js
+++
@@ -1,7 +0,0 @@
-const Aljabar = require('./AljabarLib');
-
-const akar = Aljabar.AkarPersamaanKuadrat([1, -3, -10]);
-console.log('Akar Persamaan Kuadrat:', akar);
-
-const hasilKuadrat = Aljabar.HasilKuadrat([2, -3]);
-console.log('Hasil Kuadrat:', hasilKuadrat);
