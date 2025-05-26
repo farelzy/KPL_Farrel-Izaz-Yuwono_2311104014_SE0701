366def086630f7ef03fa12713db8d16dcfebb4c5
IzazRz <73918830+farizaw123@users.noreply.github.com>
2025-05-26 19:10:29 +0700

    modul 10

---
+++ 10_LIBRARY/JURNAL/main.js
@@ -0,0 +1,6 @@
+const MathLib = require('./MathLib');
+
+console.log('FPB:', MathLib.FPB(60, 45));
+console.log('KPK:', MathLib.KPK(12, 8));
+console.log('Turunan:', MathLib.Turunan([1, 4, -12, 9]));
+console.log('Integral:', MathLib.Integral([4, 6, -12, 9]));
