412e768e7fba5f768944d90a8db207b810c3629f
IzazRz <73918830+farizaw123@users.noreply.github.com>
2025-05-26 19:15:44 +0700

    modul 10

---
+++ 10_LIBRARY/TP/main_Aljabar.js
@@ -0,0 +1,7 @@
+const Aljabar = require('./AljabarLib');
+
+const akar = Aljabar.AkarPersamaanKuadrat([1, -3, -10]);
+console.log('Akar Persamaan Kuadrat:', akar);
+
+const hasilKuadrat = Aljabar.HasilKuadrat([2, -3]);
+console.log('Hasil Kuadrat:', hasilKuadrat);
